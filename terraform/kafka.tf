639f1ab9f9d03b858781058ba273461851cb0a73
vasilisanikonova <nikonovavasilisa19@gmail.com>
2025-01-21 20:25:04 +0300

    Обновлены ранее добавленные схемы. Добавлены недостающие схемы. Решение первых трех заданий (задание 4 - не решалось)

--- terraform/kafka.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "kafka" {
-    repository = "oci://registry-1.docker.io/bitnamicharts"
-    name = "kafka"
-    chart = "kafka"
-    version = "30.0.0"
-    namespace  = "kafka"
-    create_namespace = true
-}
